841ba24cb08f734244b718f0f22e1b48704126c1
insorker <insorker@qq.com>
2021-02-22 09:34:21 +0800

    修改了测试文件

--- charimg_test.h
+++ charimg_test.h
@@ -4,18 +4,24 @@
 void test_chim_cmd() {
 	FILE* fp;
 	fopen_s(&fp, "img/test1.bmp", "rb");
-	chim_cmd(fp, 1, 2);
-	// chim_cmd(fp, 1, 2);
-	// chim_cmd(fp, 3, 1);
-	// chim_cmd(fp, 10, 3);
+	if (fp != NULL) {
+		chim_cmd(fp, 1, 2);
+		// chim_cmd(fp, 1, 2);
+		// chim_cmd(fp, 3, 1);
+		// chim_cmd(fp, 10, 3);
+		fclose(fp);
+	}
 }
 
 void test_chim_file() {
 	FILE* fpin, * fpout;
 	fopen_s(&fpin, "img/test1.bmp", "rb");
 	fopen_s(&fpout, "txt/test1.txt", "wb");
-	chim_file(fpin, 1, 2, fpout);
-	//chim_file(fpin, 1, 2, fpout);
-	//chim_file(fpin, 3, 1, fpout);
-	//chim_file(fpin, 10, 3, fpout);
+	if (fpin != NULL && fpout != NULL) {
+		chim_file(fpin, 1, 2, fpout);
+		//chim_file(fpin, 1, 2, fpout);
+		//chim_file(fpin, 3, 1, fpout);
+		//chim_file(fpin, 10, 3, fpout);
+		fclose(fpin), fclose(fpout);
+	}
 }
